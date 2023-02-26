5dd2c2c0f766605d5ee0d85f25222c8806736250
Kacper Bylicki <kacper@bylicki.dev>
2023-02-26 17:25:18 +0100

    feat(unit-test): add basic unit testing setup

---
+++ apps/frontend/test/App.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import App from '../src/App';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App />);
+
+    screen.debug();
+  });
+});
