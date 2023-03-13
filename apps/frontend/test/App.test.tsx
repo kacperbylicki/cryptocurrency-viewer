c5ee8176e62098aec87a11bcba35e59689e67279
marek_gerszendorf <marek.gerszendorf@wp.pl>
2023-03-13 19:55:44 +0100

    chore(frontend): move test directory to root of the project

--- apps/frontend/test/App.test.tsx
+++ apps/frontend/test/App.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import App from '../App';
+import App from '../src/App';
 
 describe('App', () => {
   it('renders headline', () => {
