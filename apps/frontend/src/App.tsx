8ddebc2f7cec88191cbb5285bb1a2fa97d3ce206
marek_gerszendorf <marek.gerszendorf@wp.pl>
2023-04-02 18:01:03 +0200

    feat(frontend): created root and changed nesting

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import './assets/styles/App.scss';
+import Root from './Root';
+
 function App(): JSX.Element {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Root />
+    </div>
+  );
 }
 
 export default App;
