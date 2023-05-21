d6a4d3b72d755f0d997af4f4791ce995fdea546e
Kacper Bylicki <kacper@bylicki.dev>
2023-05-21 10:07:04 +0200

    fix(backend): cryptocurrency news mock

--- apps/cryptocurrency-service/test/mocks/cryptocurrency-news.mock.ts
+++ apps/cryptocurrency-service/test/mocks/cryptocurrency-news.mock.ts
@@ -8,9 +8,11 @@ export const mockCryptocurrencyNews: CryptocurrencyNews[] = [
     name: 'Article 1',
     url: 'https://example.com/article1',
     image: {
-      contentUrl: 'test-image',
-      width: 100,
-      height: 100,
+      thumbnail: {
+        contentUrl: 'test-image',
+        width: 100,
+        height: 100,
+      },
     },
     description: 'test-description',
     datePublished: '2021-07-01T00:00:00.000Z',
@@ -20,9 +22,11 @@ export const mockCryptocurrencyNews: CryptocurrencyNews[] = [
     name: 'Article 2',
     url: 'https://example.com/article2',
     image: {
-      contentUrl: 'test-image',
-      width: 100,
-      height: 100,
+      thumbnail: {
+        contentUrl: 'test-image',
+        width: 100,
+        height: 100,
+      },
     },
     description: 'test-description',
     datePublished: '2021-07-01T00:00:00.000Z',
